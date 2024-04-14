7667c630a27cbfa2b2a74228edc16d331abf08e3
Staval <stalkervaultboy@gmail.com>
2024-04-15 02:04:49 +0400

    Готовая 3 лаба

---
+++ Lab_3/Lab_3.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <random>
+#include <boost/multiprecision/cpp_int.hpp>
+
+using namespace std;
+using boost::multiprecision::cpp_int;
+
+
+void Read_Data(vector<int>& arr, int size, const string& str)
+{
+    arr.resize(size);
+
+    ifstream data;
+    data.open(str);
+
+    if(data.is_open()) {
+        for(int & i : arr) {
+                data >> i;
+        }
+    }
+
+    data.close();
+}
+
+
+bool Is_Prime(cpp_int n, vector<int>& arr) {
+    if (n == 1 || n == 0) return false;
+
+    for(int & i : arr) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+unsigned long long Generate_Number(vector<int>& arr) {
+    unsigned long long N = 0;
+
+    while (true) {
+        std::random_device rd;
+        std::mt19937_64 gen(rd());
+
+        std::uniform_int_distribution<unsigned long long> dis;
+
+        N = dis(gen);
+
+        string Num = to_string(N);
+        Num[0] = '1';
+        Num[Num.size() - 1] = '1';
+        N = stoull(Num);
+
+        if (Is_Prime(N, arr)) {
+            cout << N << endl;
+            return N;
+        }
+    }
+}
+
+
+cpp_int GCD(cpp_int x, cpp_int y) {
+    cpp_int g;
+    g = y;
+    while (x > 0) {
+        g = x;
+        x = y % x;
+        y = g;
+    }
+
+    return g;
+}
+
+
+cpp_int Pow_Mod(cpp_int base, cpp_int exponent, cpp_int mod) {
+    cpp_int x (1);
+    cpp_int y (base);
+    while (exponent > 0) {
+        if (exponent % 2 == 1)
+            x = (x * y) % mod;
+        y = (y * y) % mod;
+        exponent = exponent / 2;
+    }
+    return x % mod;
+}
+
+
+bool Ferma_Test(cpp_int N, int iterations) {
+    for (int i = 0; i < iterations; ++i) {
+        cpp_int a (rand() % N + 1);
+        cpp_int nod = GCD(a, N);
+        if (nod != 1)
+            return false;
+
+        a = Pow_Mod(a, N-1, N);
+        if (a != 1)
+            return false;
+    }
+    return true;
+}
+
+
+int main() {
+    srand(time(nullptr));
+    vector<int> arr;
+    Read_Data(arr, 303, R"(C:\Users\Staval\CLionProjects\Cryptography\Lab_3\Simple_nums.txt)");
+
+    string Num = "2378722555942580437748473932262843474336241766370302304516933279431755288790723383256879373500755979"
+    "3322366772080177571997428806566480649860797050230962648535593609516556822487916337419976895937637619"
+    "5753658195140051275782298822604303145423104616773980711963067501960023289349641123537747609039016523";
+    boost::multiprecision::cpp_int n(Num);
+
+    cpp_int N (6609719587371582529);
+
+    bool f = Is_Prime(n, arr);
+
+    if (!f) {
+        cout << "Not a prime number" << endl;
+        return 0;
+    }
+
+    f = Ferma_Test(n, 5);
+
+    if (!f) {
+        cout << "Not a prime number" << endl;
+        return 0;
+    }
+    else {
+        cout << "A prime number" << endl;
+    }
+
+    return 0;
+}
